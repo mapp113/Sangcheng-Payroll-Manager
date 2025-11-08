2a0f7d972bb52865a3afc12208868e5241d246d3
LEGION <minhpvahe170195@fpt.edu.vn>
2025-11-08 15:50:35 +0700

    fix leave balance, LeaveRequestValidator

--- backend/src/main/java/com/g98/sangchengpayrollmanager/repository/LeaveBalanceRepository.java
+++ backend/src/main/java/com/g98/sangchengpayrollmanager/repository/LeaveBalanceRepository.java
@@ -2,15 +2,16 @@ package com.g98.sangchengpayrollmanager.repository;
 
 import com.g98.sangchengpayrollmanager.model.entity.LeaveBalance;
 import com.g98.sangchengpayrollmanager.model.entity.LeaveType;
-import com.g98.sangchengpayrollmanager.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface LeaveBalanceRepository  extends JpaRepository<LeaveBalance, Long> {
-    Optional<LeaveBalance> findByUserAndLeaveTypeAndYear(User user, LeaveType leaveType, int year);
 
-    Optional<LeaveBalance> findByEmployeeCode(String employeeCode);
+    @Query("SELECT lb FROM LeaveBalance lb WHERE lb.user.employeeCode = :employeeCode")
+    Optional<LeaveBalance> findByEmployeeCode(@Param("employeeCode")  String employeeCode);
 }
